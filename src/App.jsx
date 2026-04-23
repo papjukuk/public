8f838d30fe302901191646b733001b2d7493e773
papjukuk <qkqwnrjr1203@naver.com>
2026-04-23 17:29:52 +0900

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   /// 추가 - 마커 생성
     const iconFeature = new Feature({
       geometry: new Point([127.033045, 37.600488]),
-      name: 'Java 기반 시스템 유지보수 업무를 수행한 경험이 있습니다. 처음에는 매주 정해진 일정에 맞춰 데이터를 반영해야 하는 업무가 부담으로 느껴지고, 시스템 구조를 충분히 이해하지 못한 상태에서 일을 진행하는 것도 쉽지 않았습니다. 혼자 고민하기보다는 선임자에게 먼저 다가가 팀에 자연스럽게 녹아드는 방법을 선택했습니다. 선임분들이 번거롭게 여기는 잡무와 보조 업무를 주도적으로 맡으며, 그 과정에서 선임자와의 소통이 자연스럽게 늘어났고 시스템 구조와 업무 흐름을 깊이 이해할 수 있었습니다. 또한 주간보고와 중간보고를 통해 진행 상황을 공유하는 습관을 익히며, 일정에 맞춰 안정적으로 업무를 운영하는 것이 중요하다는 것을 배웠습니다. 이러한 경험을 통해 배운 적극적인 태도로, 모르는 부분은 먼저 질문하며 배우고 남들이 꺼리는 업무도 솔선수범해 맡으며 팀에 자연스럽게 녹아드는 구성원이 되겠습니다',
+      name: 'Failed to load module script: Expected a JavaScript-or-Wasm module script but the server responded with a MIME type of "text/jsx". Strict MIME type checking is enforced for module scripts per HTML spec.',
     })
     const iconStyle = new Style({
       image: new Icon({
